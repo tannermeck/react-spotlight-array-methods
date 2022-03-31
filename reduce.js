78b4cc5136aea8751735f5ac64c557a71a018162
TANNER MECK <steve.meck246@gmail.com>
2022-03-31 11:48:14 -0700

    all tests passing

--- reduce.js
+++ reduce.js
@@ -2,9 +2,23 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, brand) => {
+    brand.shoes.map((shoe) => acc.push(shoe));
+    return acc;
+  }, []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const total = dogs.reduce((acc, dog) => {
+    acc += dog.age;
+    console.log(acc);
+    return acc;
+  }, 0);
+  return total / dogs.length;
+  // dogs.reduce((acc, dog) => acc += dog.age, 0) / dogs.length
+  // acc defaults to first value if not defined
+};
